93a025144709e038d0452f2b3c2367af32df5fe6
Graham Cottridge <codinggc@gmail.com>
2019-09-17 20:18:37 +0100

    Add redux dev tools to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 
@@ -6,7 +6,12 @@ import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(...middlewares))
+);
 
 export const persistor = persistStore(store);
 
