56266a3733acf38ec1a654810dc7e2c0756bab73
Graham Cottridge <codinggc@gmail.com>
2019-09-27 23:46:36 +0100

    Update custom button component function for selected styles

--- src/components/custom-button/custom-button.styles.jsx
+++ src/components/custom-button/custom-button.styles.jsx
@@ -1,30 +1,44 @@
 import styled, { css } from "styled-components";
 
-export const invertedButtonStyles = css`
-  &.inverted {
+const buttonStyles = css`
+  background-color: black;
+  color: white;
+  border: none;
+  &:hover {
     background-color: white;
     color: black;
     border: 1px solid black;
+  }
+`;
 
-    &:hover {
-      background-color: black;
-      color: white;
-      border: none;
-    }
+export const invertedButtonStyles = css`
+  background-color: white;
+  color: black;
+  border: 1px solid black;
+
+  &:hover {
+    background-color: black;
+    color: white;
+    border: none;
   }
 `;
 
 export const googleSignInStyles = css`
-  &.google-sign-in {
-    background-color: #4285f4;
-    color: white;
+  background-color: #4285f4;
+  color: white;
 
-    &:hover {
-      background-color: #357ae8;
-      border: none;
-    }
+  &:hover {
+    background-color: #357ae8;
+    border: none;
   }
 `;
+
+const getButtonStyles = props => {
+  if (props.isGoogleSignIn) {
+    return googleSignInStyles;
+  }
+  return props.inverted ? invertedButtonStyles : buttonStyles;
+};
 
 export const CustomButtonContainer = styled.button`
   min-width: 65px;
@@ -34,19 +48,14 @@ export const CustomButtonContainer = styled.button`
   line-height: 15px;
   padding: 0 15px 0 15px;
   font-size: 15px;
-  background-color: black;
-  color: white;
+
   text-transform: uppercase;
   font-family: "Open Sans Condensed";
   font-weight: bolder;
-  border: none;
+
   cursor: pointer;
   display: flex;
   justify-content: center;
 
-  &:hover {
-    background-color: white;
-    color: black;
-    border: 1px solid black;
-  }
+  ${getButtonStyles}
 `;
