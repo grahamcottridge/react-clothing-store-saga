9f079388572e10f98d348ee5188499d640d5b930
Graham Cottridge <codinggc@gmail.com>
2019-10-14 11:28:42 +0100

    Update checkout'

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -35,7 +35,7 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className="total">
-      <span>TOTAL: ${total}</span>
+      <span>TOTAL: £{total}</span>
     </div>
     <div className="test-warning">
       *Please use the following test credit card for payments*
