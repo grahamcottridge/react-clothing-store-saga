db9ff251a8e38aa61a6f8403bc0729560df8fcb9
Graham Cottridge <codinggc@gmail.com>
2019-11-02 23:45:15 +0000

    Add shop data to firebase

--- src/App.js
+++ src/App.js
@@ -32,11 +32,9 @@ class App extends React.Component {
             id: snapShot.id,
             ...snapShot.data()
           });
-          // console.log(this.state);
         });
-      } else {
-        setCurrentUser(userAuth);
       }
+      setCurrentUser(userAuth);
     });
   }
 
