67f36ad074bd31883cd10e909f23fe5eafa0f6a9
Graham Cottridge <codinggc@gmail.com>
2019-11-16 20:22:15 +0000

    Implement root saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 import createSagaMiddleware from "redux-saga";
 
-import { fetchCollectionsStart } from "./shop/shop.sagas";
+import rootSaga from "./root-saga";
 
 import rootReducer from "./root-reducer";
 
@@ -22,7 +22,7 @@ export const store = createStore(
   composeEnhancers(applyMiddleware(...middlewares))
 );
 
-sagaMiddleware.run(fetchCollectionsStart);
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
 
